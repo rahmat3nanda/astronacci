1ebdf9dc54c29b607089f1341bf8ec1b8adc4c9a
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-18 19:43:00 +0700

    Create Splash Page

---
+++ lib/page/splash_page.dart
@@ -0,0 +1,95 @@
+/*
+ * *
+ *  * splash_page.dart - astronacci
+ *  * Created by Rahmat Trinanda (rahmat3nanda@gmail.com) on 09/18/2024, 19:38
+ *  * Copyright (c) 2024 . All rights reserved.
+ *  * Last modified 09/18/2024, 19:38
+ *
+ */
+
+import 'package:astronacci/bloc/auth/auth_bloc.dart';
+import 'package:astronacci/common/constants.dart';
+import 'package:astronacci/model/app/singleton_model.dart';
+import 'package:astronacci/tool/helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AuthBloc _bloc;
+  late Helper _helper;
+
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    SingletonModel.withContext(context);
+    _bloc = BlocProvider.of<AuthBloc>(context);
+    _helper = Helper();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _setup();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _setup() async {
+    await _controller.forward();
+    _bloc.add(const AuthLoadEvent());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener(
+      bloc: _bloc,
+      listener: (c, s) {
+        if (s is AuthLoadSuccessState) {
+          // TODO: Routing
+        } else if (s is AuthLoadFailedState) {
+          // TODO: Routing
+        }
+      },
+      child: BlocBuilder(
+        bloc: _bloc,
+        builder: (c, s) {
+          return Scaffold(
+            body: FadeTransition(
+              opacity: CurvedAnimation(
+                parent: _controller,
+                curve: Curves.easeOut,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Transform.translate(
+                    offset: const Offset(-12, 0),
+                    child: SvgPicture.asset(
+                      AppIcon.astronacci,
+                      width: 152,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
